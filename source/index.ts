af0ffc907c9dbcce4ff19313084f7a32773f4b15
officeukraine\d.samborskyi <d.samborskyi@temabit.com>
2020-08-12 22:36:25 +0300

    export all functions to the outside

--- source/index.ts
+++ source/index.ts
@@ -1,1 +1,4 @@
-console.log(`Start project`);
+export { EndPointAPI } from './services/api.service';
+export { EndPointService } from './services/end.point.service';
+export { EndPoint } from './decorators/end.point.decorator';
+export { EndPointMethod } from './decorators/method.decorator';
