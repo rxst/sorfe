dfd65570c3754011f45fd778f2bd7da7f4a8f83f
d.samborskyi <dimasamborskyi@gmail.com>
2021-01-10 21:35:39 +0200

    Created decorator service

--- source/executor/decorators/executor.method.decorator.ts
+++ source/executor/decorators/executor.method.decorator.ts
@@ -1,2 +1,20 @@
 // TODO: Decorator must to mark method as executable
 // TODO: Decorator must to have functionality for validating inner data
+interface IExecutorMethodDecoratorOptions {
+    name: string
+}
+
+export function Method(options: IExecutorMethodDecoratorOptions) {
+    return (target: Object, propertyKey: string, descriptor: PropertyDescriptor | undefined) => {
+        if(descriptor === undefined) {
+            descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+        }
+
+        if (descriptor) {
+            descriptor.value.prototype.__isSorfeMethod = true;
+            descriptor.value.prototype.__sorfeMethodName = options.name;
+        }
+
+        return descriptor;
+    };
+}
