6f0046a39515fa76fd591b6cb594d27389b9e7fd
d.samborskyi <dimasamborskyi@gmail.com>
2021-01-10 17:15:19 +0200

    Created electron impl of transport

---
+++ source/transport/transport.ipc.ts
@@ -0,0 +1,17 @@
+import { TransportAbstract } from "./transport.abstract";
+import { IResponseReject, IResponseSuccess } from "../validators/interfaces/response.interfaces";
+import { IRequest } from "../validators/interfaces/request.interfaces";
+import { ipcMain, IpcMainEvent } from 'electron';
+
+export class TransportIpc extends TransportAbstract {
+    private static _ipcChannel: string = process.env.IPC_CHANNEL || 'sorfe';
+
+    transportMessageHandler(listener: (message: IRequest, send: (response: (IResponseSuccess | IResponseReject)) => void) => void): void {
+        ipcMain.on(TransportIpc._ipcChannel, (event: IpcMainEvent, request: IRequest) => {
+            listener(request, async (response: IResponseSuccess | IResponseReject) => {
+                event.reply(response);
+            })
+        });
+    }
+
+}
