e12a2f6c1e7b0ce7713a9da8ce7ee63057f90b01
d.samborskyi <dimasamborskyi@gmail.com>
2021-01-10 21:35:55 +0200

    Created decorator service

--- source/executor/decorators/executor.service.decorator.ts
+++ source/executor/decorators/executor.service.decorator.ts
@@ -1,1 +1,17 @@
 // TODO: Decorator must to mark a service as executed service
+import { EventEmitter } from "events";
+
+interface IExecutorServiceDecoratorOptions {
+    name: string
+}
+
+export function Service(options: IExecutorServiceDecoratorOptions): ClassDecorator {
+    return (target) => {
+        if (target.prototype instanceof EventEmitter) throw Error('EndPoint decorator can`t work with Event Emitter class');
+
+        target.prototype.__isSorfeService = true;
+        target.prototype.__sorfeServiceName = options.name;
+
+        return target;
+    }
+}
