e77944000276a30786c7e1e1f89534f83b4ec50a
d.samborskyi <dimasamborskyi@gmail.com>
2021-01-20 00:58:04 +0200

    Updated executor service for use executor endpoint

---
+++ source/executor/executor.service.spec.ts
@@ -0,0 +1,9 @@
+import { ExecutorService } from './executor.service';
+
+describe('ExecutorService', () => {
+    describe('creation', () => {
+        test('', () => {
+
+        })
+    })
+})
--- source/executor/executor.service.ts
+++ source/executor/executor.service.ts
@@ -1,3 +1,24 @@
-// TODO: Make the class for control which map all executable services and they methods
-// TODO: Class make a service and they methods tree
-// TODO: Class make a execute without context trouble
+import { IExecutor, IExecutorServiceOptions } from "./interfaces/executor.interfaces";
+import { ExecutorEndpoint } from "./endpoint/executor.endpoint";
+
+export class ExecutorService implements IExecutor {
+    private _endpoints: Map<string, ExecutorEndpoint> = new Map<string, ExecutorEndpoint>();
+
+    constructor(options: IExecutorServiceOptions) {
+        options.services
+            .map(c => new c())
+            .filter(initClass => !!initClass.__proto__.__isSorfeService && !!initClass.__proto__.__sorfeServiceName)
+            .forEach(endPoint => this._endpoints.set(endPoint.__proto__.__sorfeServiceName, new ExecutorEndpoint(endPoint)))
+    }
+
+    public async call<T>(service: string, method: string, ...params: any): Promise<T> {
+        const endpoint = this._endpoints.get(service)
+
+        if (endpoint) {
+            return endpoint.call<T>(method, ...params)
+        } else {
+            throw new Error(`Service ${service} not registered as Endpoint`);
+        }
+
+    }
+}
