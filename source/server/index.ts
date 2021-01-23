79925f17d7f8e4a83c81da814965d9afb13daa85
d.samborskyi <dimasamborskyi@gmail.com>
2021-01-23 19:29:58 +0200

    Move impl to separated directory

--- source/server/index.ts
+++ source/server/index.ts
@@ -1,5 +1,5 @@
 export * from './transport/transport.abstract';
-export * from './transport/transport.ipc';
+export * from './transport/impl/transport.ipc';
 export * from './executor/decorators/executor.method.decorator';
 export * from './executor/decorators/executor.service.decorator';
 export * from './control/control.service';
